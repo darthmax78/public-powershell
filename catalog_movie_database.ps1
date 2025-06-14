c76e5a72611a32d7c5dcf384fe316d6103a878d9
darthmax78 <magnus.witzik@opti-net.at>
2025-06-14 16:32:00 +0200

    catalog_movie_database.ps1: V. 1.2 Further Bugfixes: Preps for Changing detection and further Reporting

--- catalog_movie_database.ps1
+++ catalog_movie_database.ps1
@@ -37,7 +37,9 @@
                 1.1: Rework of the Audio Channel Output (Surround Detection)
                         - Code optimization with additional functions
                         - Smaller fix for Resolution Detection
-                1.2
+                1.2: Detect Changes on new or changed files
+                        - Implementing the new Function
+                        - Remaining Time Calculation insert in Progress Bar
 
 
     .AUTHOR
@@ -51,7 +53,8 @@ function check_variables
     Write-Host "Checking Variables..." -ForegroundColor Cyan
     $global:movie_location          = "LOCATION_MOVIES"
     $global:movie_csv_file          = "LOCATION_CSV_EXPORT"
-    $global:date_scanning           = (Get-Date -Format "yyyy-MM-dd_HH-mm-ss")  
+    $global:date_scanning           = (Get-Date -Format "yyyy-MM-dd_HH-mm-ss")
+    $global:actual_catalogue        = Import-Csv -Path ((Get-ChildItem -Path "$global:movie_csv_file\*" -File -Include *.csv | Where-Object { $_.Name -match "\AMovie-Report" } | Sort-Object LastWriteTime | Select-Object -First 1).FullName) -Delimiter ";" -Encoding utf8BOM
 
     # Checking if the Get-MediaInfo Module is installed
     try
@@ -68,11 +71,18 @@ function check_variables
             Write-Host "The URL to the Module is copied to the Clipboard: " -ForegroundColor Yellow -NoNewline; Write-Host $url -ForegroundColor Cyan
             exit
         }
+        else { }
     }
 
     # if the Module is installed, then we can continue
     Write-Host "Get-MediaInfo Module is installed." -ForegroundColor Green    
     $global:all_movies              = Get-ChildItem -Path $global:movie_location -Recurse -File -Include *.mkv, *.mp4, *.avi, *.mov, *.wmv, *.flv, *.webm | Sort-Object -Property Name
+}
+
+# Detect Changes, when there was a catalog prevously created, to fasten up the new Detection Speed
+function detect_changes
+{
+    $list_reference = $global:actual_catalogue | Select-Object "Film Titel","Auflösung"
 }
 
 # The Main Function, which analyzes the movies and stores the information in a csv file.
@@ -88,12 +98,13 @@ function analyzing_movies
         Write-Host "Found $($global:all_movies.Count) Movies in the specified location." -ForegroundColor Green
         $counter                = 0
         $global:movie_table     = @()
+        $start_time             = Get-Date
 
         $global:all_movies | ForEach-Object `
         {
             $counter++
             $counter_percent    = [math]::Round(($counter / $global:all_movies.Count) * 100, 2)
-            Write-Progress -Activity "Analyzing Movies" -Status "Processing $($_.Name) ($counter_percent%)" -PercentComplete $counter_percent
+
             $movie_file_info    = $_
             $movie_media        = Get-MediaInfo -Path $movie_file_info.FullName -ErrorAction SilentlyContinue
             $bild_weite         = $movie_media.Width
@@ -230,12 +241,23 @@ function analyzing_movies
             "Film Erstellt"     = $date_created 
             "Programm"          = Get-MediaInfoValue -Path $movie_file_info.FullName -Kind General -Parameter "Encoded_Application/String"
             "Film Größe"        = human_readable -Bytes ($movie_file_info.Length)
+            "Datei Name"        = $movie_file_info.Name
+            "Datei Erstellt"    = $movie_file_info.LastWriteTime
             }
 
             $global:movie_table += $movie_info
             $movie_info | Export-Csv -Path "$global:movie_csv_file\Movie-Report_$global:date_scanning.csv" -Append -NoTypeInformation -Encoding utf8BOM -Delimiter ";"
 
-            Clear-Variable -Name bild_weite, bild_hoehe, auflösung, standard, year, title, edition, date_created- ErrorAction SilentlyContinue
+            
+            # Time Calculation for the Remaining Time of the Progress
+            $time_elapsed           = ((Get-Date) - $start_time).TotalSeconds
+            $movie_remaining        = ($global:all_movies).Count-$counter
+            $time_average_movie     = $time_elapsed/$counter
+            $time_remaining         = ([TIMESPAN]::FromSeconds( $movie_remaining*$time_average_movie )).ToString("hh\:mm\:ss")
+
+            Write-Progress -Activity "Analyzing Movies" -Status "Processing $($_.Name) ($counter_percent%) ($time_remaining)" -PercentComplete $counter_percent
+
+            Clear-Variable -Name bild_weite, bild_hoehe, auflösung, standard, year, title, edition, date_created -ErrorAction SilentlyContinue
             Clear-Variable -Name audio_channel_1, audio_channel_2 -Scope Global -ErrorAction SilentlyContinue
         }
     }
@@ -243,7 +265,7 @@ function analyzing_movies
 
 function show_movie_report
 {
-    $global:movie_table | Format-Table -Property * -AutoSize | Out-String -Stream | ForEach-Object `
+    $global:movie_table | Format-Table -Property $global:report_property -AutoSize | Out-String -Stream | ForEach-Object `
     {
         $studiostatecolor = `
             if ($_ -match "HD 1080") {@{'ForegroundColor' = 'Blue'}}
@@ -257,5 +279,6 @@ function show_movie_report
 }
 
 check_variables
+# detect_changes
 analyzing_movies
 show_movie_report
